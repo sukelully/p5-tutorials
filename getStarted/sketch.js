93889c28b272dcb7cc09707a20d40096ebe2045c
Luke Sullivan <sully097@gmail.com>
2025-03-16 18:17:28 +0000

    Basic sketch

--- getStarted/sketch.js
+++ getStarted/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
 }
 
 function draw() {
-    background(220);
+    background(135, 206, 235);
+
+    // Sun
+    fill('yellow');
+    stroke('orange');
+    strokeWeight(20);
+    circle(550, 250, 100);
+
+    // Ground
+    fill('green');
+    stroke('black')
+    rect(-20, height/2+100, width+40, height);
 }
