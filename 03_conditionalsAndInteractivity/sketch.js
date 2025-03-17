edfc0625d6878716e4aaf4481775b9268a357831
Luke Sullivan <sully097@gmail.com>
2025-03-17 13:04:40 +0000

    03 done

--- 03_conditionalsAndInteractivity/sketch.js
+++ 03_conditionalsAndInteractivity/sketch.js
@@ -1,7 +1,54 @@
+// Initial sun position
+let sunHeight = 600;
+
+// Color change variables
+let redVal = 0;
+let greenVal = 0;
+
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(600, 400);
+}
+
+function drawSun() {
+    fill(255, 135, 5, 60);
+    circle(300, sunHeight, 180);
+    fill(255, 100, 0, 100);
+    circle(300, sunHeight, 140);
+}
+
+function drawMountains() {
+    fill(110, 50, 18);
+    triangle(200, 400, 520, 253, 800, 400);
+    fill(110, 95, 20);
+    triangle(200, 400, 520, 253, 350, 400);
+
+    fill(150, 75, 0);
+    triangle(-100, 400, 150, 200, 400, 400);
+    fill(100, 50, 12);
+    triangle(-100, 400, 150, 200, 0, 400);
+
+    fill(150, 100, 0);
+    triangle(200, 400, 450, 250, 800, 400);
+    fill(120, 80, 50);
+    triangle(200, 400, 450, 250, 300, 400);
 }
 
 function draw() {
-    background(0);
+    background(redVal, greenVal, 0);
+
+    drawSun();
+
+    drawMountains();
+
+    if (sunHeight > 130) {
+        sunHeight -= 2;
+
+        if (sunHeight < 480) {
+            redVal +=4;
+            greenVal += 1;
+        } else {
+            redVal -= 2;
+            greenVal -= 4;
+        }
+    }
 }
